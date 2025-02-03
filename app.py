64ded6e6371ee6449d37cee208079e6bab548a9d
Onelots <onelots@onelots.fr>
2025-02-04 00:11:57 +0100

    testers: introduce new website section
    enhanced visuals too, and added various devices. (Seriously, what am I doing ?
    
    Signed-off-by: Onelots <onelots@onelots.fr>

--- app.py
+++ app.py
@@ -20,9 +20,17 @@ unofficial_devices = {
     "Oneplus Nord N10 5G\n(Work in progress.)": {"codename": "billie", "image": "https://oasis.opstatics.com/content/dam/oasis/page/billie/N10-Frame11.png", "download": "https://evox.onelots.fr/download"},
     "Oneplus 5\n(Work in progress.)": {"codename": "cheeseburger", "image": "https://cdn.opstatics.com/store/20170907/assets/images/support/support-list/model-specs-list/details/5-black.png", "download": "https://evox.onelots.fr/download"},
     "Oneplus 5T\n(Work in progress.)": {"codename": "dumpling", "image": "https://cdn.opstatics.com/store/20170907/assets/images/support/support-list/model-specs-list/details/5-black.png", "download": "https://evox.onelots.fr/download"},
+    "Oneplus 7 \n(Work in progress.)": {"codename": "guacamoleb","image": "https://media2.gsm55.com/media/device/4447/4447.png", "download": "https://evox.onelots.fr/download"},
+    "Oneplus 7 Pro\n(Work in progress.)": {"codename": "guacamole", "image": "https://cdn.revendo.com/media/08/63/34/1662390983/oneplus-7-pro-nebula-blue-guenstig-gebraucht-kaufen.png.png", "download": "https://evox.onelots.fr/download"},
     "Oneplus 7T\n(Work in progress.)": {"codename": "hotdogb", "image": "https://cdn.revendo.com/media/86/84/34/1662392047/oneplus-7t-glacier-blue-guenstig-gebraucht-kaufen.png.png", "download": "https://evox.onelots.fr/download"},
     "Oneplus 7T Pro\n(Work in progress.)": {"codename": "hotdog", "image": "https://www.gizmochina.com/wp-content/uploads/2019/09/oneplus_7t_pro_1_3.png", "download": "https://evox.onelots.fr/download"},
     "Xiaomi Mi A3\n(Work in progress.)": {"codename": "laurel_sprout", "image": "https://i01.appmifile.com/webfile/globalimg/T/20190718094400.png", "download": "https://evox.onelots.fr/download"},
+    "Poco M2 Pro": {"codename": "gram (MiAtoll serie)", "image": "https://c0.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2020/07/xiaomi-poco-m2-pro-frandroid-2020-768x768.png", "download": "https://evox.onelots.fr/download"},
+    "Xiaomi Redmi Note 9S": {"codename": "curtana (MiAtoll serie)", "image": "https://media2.gsm55.com/media/device/4575/4575.png", "download": "https://evox.onelots.fr/download"},
+    "Xiaomi Redmi Note 9 Pro": {"codename": "joyeuse (MiAtoll serie)", "image": "https://media2.gsm55.com/media/device/4572/4572.png", "download": "https://evox.onelots.fr/download"},
+    "Xiaomi Redmi Note 9 Pro (India)": {"codename": "curtana (MiAtoll serie)", "image": "https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1595220196.89229402!400x400!85.png", "download": "https://evox.onelots.fr/download"},
+    "Xiaomi Redmi Note 9 Pro Max": {"codename": "excalibur (MiAtoll serie)", "image": "https://media2.gsm55.com/media/device/4572/4572.png", "download": "https://evox.onelots.fr/download"},
+    "Xiaomi Redmi Note 10 lite": {"codename": "curtana (MiAtoll serie)", "image": "https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1588937747.86846999!400x400!85.png", "download": "https://evox.onelots.fr/download"},
 }
 
 @app.route('/')
@@ -76,6 +84,43 @@ def downloads():
             total_size = check_size('downloads')
             folder_size = check_size(current_path)
             return render_template('downloads.html', items=items, current_path=path, parent_path=parent_path, total_size=total_size, folder_size=folder_size)
+        else:
+            return send_file(current_path, as_attachment=True, mimetype="application/octet-stream")
+    except PermissionError:
+        abort(403)
+
+
+# Yes I'm lazy so I'll just copy/paste
+@app.route('/testers')
+def testers():
+    path = request.args.get('path', '')
+    base_dir = os.environ.get('DOWNLOAD_DIR', 'testers')
+    current_path = os.path.join(base_dir, path)
+
+    try:
+        if not os.path.exists(current_path):
+            abort(404)
+
+        if not os.path.realpath(current_path).startswith(os.path.realpath(base_dir)):
+            abort(403)
+
+        items = []
+        if os.path.isdir(current_path):
+            for item in os.listdir(current_path):
+                item_path = os.path.join(current_path, item)
+                is_dir = os.path.isdir(item_path)
+                items.append({
+                    'name': item,
+                    'is_dir': is_dir,
+                    'path': os.path.join(path, item) if path else item
+                })
+
+            items.sort(key=lambda x: (not x['is_dir'], x['name'].lower()))
+
+            parent_path = os.path.dirname(path) if path else None
+            total_size = check_size('testers')
+            folder_size = check_size(current_path)
+            return render_template('testers.html', items=items, current_path=path, parent_path=parent_path, total_size=total_size, folder_size=folder_size)
         else:
             return send_file(current_path, as_attachment=True, mimetype="application/octet-stream")
     except PermissionError:
